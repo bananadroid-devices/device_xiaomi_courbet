fa68b632d9e9526c421c691648883f250f496231
Rizky Benggolo <travarilo@gmail.com>
2023-11-26 21:36:52 +0700

    courbet: Rebrand to BananaDroid

--- banana_courbet.mk
+++ banana_courbet.mk
@@ -11,13 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := banana_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# BananaDroid
+BANANA_MAINTAINER := travarilo
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Recovery
+TARGET_USES_AOSP_RECOVERY := true
